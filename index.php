2db12473354977d2507841320f176bef4274870a
nikko760 <35375687+nikko760@users.noreply.github.com>
2018-01-12 16:38:08 +0100

    page tableau

---
+++ index.php
@@ -0,0 +1,113 @@
+﻿<body background="/images/bg.jpg" height="200" width="200">
+
+<style type="text/css"> 
+	table {
+	font-family: "Lato","sans-serif";   }       /* added custom font-family  */
+	 
+	table.one {                                 
+	margin-bottom: 3em;
+	border-collapse:collapse;   }  
+	 
+	td {                            /* removed the border from the table data rows  */
+	text-align: center;    
+	width: 10em;                   
+	padding: 1em;       }      
+	 
+	th {                              /* removed the border from the table heading row  */
+	text-align: left;                
+	padding: 1em;
+	background-color: #C3C6C5;       /* added a red background color to the heading cells  */
+	color: white;       }                 /* added a white font color to the heading text */
+	
+	tp {                              /* removed the border from the table heading row  */
+	text-align: left;                
+	padding: 1em;
+	background-color: #0000ff;       /* added a red background color to the heading cells  */
+	color: white;       }                 /* added a white font color to the heading text */
+	
+	tr {   
+	height: 1em;    }
+	table tr:nth-child(even) {            /* added all even rows a #eee color  */
+	    background-color: #eee;     }
+
+	table tr:nth-child(odd) {            /* added all odd rows a #fff color  */
+	background-color:#fff;      }
+
+	</style>
+
+<table width="100%">
+	<td COLSPAN="5"><h2>Tarifs à la prestations 2018<br/>
+	Ouvert de 8h00 à 20h00 7 jours/7</h2></td>
+	<tr><th width="35%">- Tarif à la préstations<br/>
+			<i>Accueil,remise de clefs, visite état de lieux, 
+			suivi du séjour, assurer le départ avec vérification du bien, 
+			fermeture
+			Rapport du séjour par email au départ des locataires</th>
+		<td>Accueil et ménage</td>
+		<td>Ménage</td>
+		<td>Entretien du jardin</td>
+		<td>*Location su linge</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>10,00 % du tarif de la location</td>
+		<td>20,00 € par heure</td>
+		<td>20,00 € de 0 à 300 m²</td>
+		<td>1 personne 10,00 € (petit lit)<br/>
+			2 personnes 18,00 € (grand lit)</td>
+	</tr>
+	
+	<tr><th>- Forfait mensuel (sans engagement)<br/>
+			<i>Accueil,remise de clefs, visite état de lieux, 
+			suivi du séjour, assurer le départ avec vérification du bien, 
+			fermeture
+			Rapport du séjour par email au départ des locataires</th>
+		<td>0 à 50 m²</td>
+		<td>51 à 100 m²</td>
+		<td>101 à 150 m²</td>
+		<td>plus de 151 m²</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>190 €/mois</td>
+		<td>240 €/mois</td>
+		<td>290 €/mois</td>
+		<td>sur devis</td>
+	</tr>
+	
+	<tr><th>- Forfait annuel (engagement 12 mois)<br/>
+			<i>Accueil,remise de clefs, visite état de lieux, 
+			suivi du séjour, séjour, assurer le départ avec vérification du bien, 
+			fermeture + entretien du jardin de - de 300 m² et ménage
+			Rapport du séjour par email au départ des locataires</th>
+		<td>0 à 50 m²</td>
+		<td>51 à 100 m²</td>
+		<td>101 à 150 m²</td>
+		<td>plus de 151 m²</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>150 €/mois</td>
+		<td>190 €/mois</td>
+		<td>240 €/mois</td>
+		<td>sur devis</td>
+	</tr>
+	
+		<tr><th>- Service locataire<br/>
+		
+		<td>Commission de service divers</td>
+		<td>Location de lit bébé</td>
+		<td>Ménage</td>
+		<td>Location de linge</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>20,00 €</td>
+		<td>10,00 €</td>
+		<td>20,00 €/heure</td>
+		<td>1 personne 10,00 € (petit lit)<br/>
+			2 personnes 18,00 € (grand lit)</td>
+	</tr>
+	<tr><th COLSPAN="5">* (La location de linge comprend ; drap de lit, house de couette, taie d'oreiller, serviette de toilette)</th></tr></i></table>
+
+
